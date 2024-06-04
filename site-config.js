fc4513079fc99628041a67cc1e32386ed1bb1ef2
ronnelpf <rpf.programmer@gmail.com>
2024-06-04 10:27:49 +0800

    first commit

---
+++ site-config.js
@@ -0,0 +1,22 @@
+const descriptionMd = `Lanao Integrated School`
+
+const description = descriptionMd
+  .replace(/\[([^\]]+)\](\([^)]+\)|\[[^\]]+\])/g, '$1')
+  .replace(/\n/g, '')
+  .replace(/\s{2,}/g, ' ')
+  .trim()
+
+module.exports = {
+  title: 'Lanao Integrated School',
+  descriptionMd,
+  description,
+  url: 'https://lanao-integrated-school.vercel.app',
+  twitterUsername: '@rpfprogrammer',
+  email: 'seongle3737@gmail.com',
+  socials: {
+    GitHub: 'https://github.com/ronnelpf/lanao-integrated-school',
+    Twitter: 'https://twitter.com/rpfprogrammer',
+  },
+  bgColor: '#1A202C',
+  themeColor: 'blue.500',
+}
