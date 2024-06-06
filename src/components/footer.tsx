a8caf046e6ae2634d15a7f05c88a2ce45c854270
ronnelpf <rpf.programmer@gmail.com>
2024-06-06 20:29:17 +0800

    add share buttons for social media

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,44 @@
 import * as React from 'react'
-import { Box, Divider, Flex, HStack, Icon, Link, Stack, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Center, Divider, Flex, HStack, Icon, Link, Stack, Text, useColorModeValue } from '@chakra-ui/react'
 import { useSocials } from '@/hooks/app'
 import Image from 'next/image'
+import {
+  FacebookIcon,
+  FacebookMessengerIcon,
+  FacebookMessengerShareButton,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+} from 'next-share'
 
 export const Footer: React.FC = () => {
   const socials = useSocials()
 
   return (
     <Box bg={useColorModeValue('gray.50', 'gray.900')} color={useColorModeValue('gray.700', 'gray.200')}>
+      <Center>
+        <Box>
+          <Text pt={6} fontSize={'md'} textAlign={'center'}>
+            Share this web app to:
+          </Text>
+        </Box>
+      </Center>
+      <Center>
+        <Box p={2}>
+          <FacebookShareButton
+            url={'https://lanao-integrated-school.vercel.app'}
+            quote={'You can do online enrollment on the Lanao Integrated School Web App'}
+            hashtag={'#LanaoIntegratedSchoolWebApp'}
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+        </Box>
+        <Box>
+          <LinkedinShareButton url={'https://lanao-integrated-school.vercel.app'}>
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+        </Box>
+      </Center>
       <Box py={10}>
         <Flex
           align={'center'}
